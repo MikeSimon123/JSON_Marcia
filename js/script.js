7a0385286c285844c653978244bb0cc8e59a6df8
MikeSimon123 <mikesimonmikesimon@gmail.com>
2025-10-21 17:30:05 -0300

    Arrumando css e adicionando exclusao

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 let formUser = document.getElementById("form-usuario")
 let tabelaUsuarios = document.getElementById("tabelaUsuarios")
 
+let nome = document.getElementById("nome-usuario");
+let email = document.getElementById("email-usuario")
+let botaoCadastrar = document.getElementById("botCad")
+let botaoCancelar = document.getElementById("botCancel")
+
+let idUsuarioEmEdicao = null
 
 formUser.addEventListener("submit", (e) => {
     e.preventDefault()
@@ -33,20 +39,32 @@ formUser.addEventListener("submit", (e) => {
 })
 
 function exibirUsuarios(users){
-    tabelaUsuarios.innerHTML = "<tr> <th>ID</th> <th>Nome</th> <th>Email</th></tr>"
+    tabelaUsuarios.innerHTML = "<tr> <th>ID</th> <th>Nome</th> <th>Email</th> <th>Ações</th></tr>"
     users.forEach((usuariosArg, index) => {
         this.linha = document.createElement("tr")
         this.linha.innerHTML = `
             <td>${index + 1 }</td>
             <td>${usuariosArg.nomeUsuario}</td>
             <td>${usuariosArg.emailUsuario}</td>
+            <td class='acoes'> <button class='edicao' id='editar${index}'>Editar</button> <button class='excluir' id='excluir${index}'>Excluir</button> </td>
         `
         
         tabelaUsuarios.appendChild(this.linha)
+
+        document.getElementById(`excluir${index}`).addEventListener('click', function(){
+                removerUsuario(index)
+        })
     })
 
    //ao carregar mostrar
    //deixar responsivo e colocar css
+}
+
+function removerUsuario(index){
+    let dados = JSON.parse(localStorage.getItem("usuarios"))
+    dados.splice(index, 1)
+    localStorage.setItem("usuarios", JSON.stringify(dados))
+    exibirUsuarios(JSON.parse(localStorage.getItem("usuarios")))
 }
 
 window.addEventListener("load", function(){
@@ -54,3 +72,8 @@ window.addEventListener("load", function(){
         exibirUsuarios(JSON.parse(localStorage.getItem("usuarios")))
     }
 })
+
+function createPopUp(){
+    let popUp = document.createElement("div")
+    
+}
